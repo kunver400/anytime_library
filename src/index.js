879528313d6c1f7f2fb631c63824cd5a1204629d
kunver400 <kunver400@gmail.com>
2018-04-27 22:04:35 +0530

    +Basic antd layout

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css?raw'; 
 import './index.css';
-import Layout from './hoc/Layout/Layout';
+import LayoutContainer from './hoc/LayoutContainer/LayoutContainer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+ReactDOM.render(<LayoutContainer />, document.getElementById('root'));
 registerServiceWorker();
