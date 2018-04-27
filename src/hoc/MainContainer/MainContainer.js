11dc780bc8b4ebd71beb17d44df5077634ba0484
kunver400 <kunver400@gmail.com>
2018-04-28 00:05:32 +0530

    granualizing components

---
+++ src/hoc/MainContainer/MainContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {Layout, Breadcrumb} from 'antd';
+
+const {Content} = Layout;
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <Layout style={{ padding: '0 24px 24px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                    Content
+          </Content>
+            </Layout>
+        )
+    }
+}
+
+export default MainContainer;
