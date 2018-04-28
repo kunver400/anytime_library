39e702502827d762ed4e20ada91926caaa09b5ed
kunver400 <kunver400@gmail.com>
2018-04-28 21:26:21 +0530

    anytime library :p

--- src/hoc/HeaderWrapper/HeaderWrapper.js
+++ src/hoc/HeaderWrapper/HeaderWrapper.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import {Layout, Menu} from 'antd';
-const {Header} = Layout;
+import { Layout, Menu, Icon } from 'antd';
+import classes from './HeaderWrapper.css'
+import logo from '../../assets/book.svg'
+const { Header } = Layout;
+const { SubMenu, ItemGroup } = Menu;
 
 class HeaderWrapper extends Component {
     render() {
         return (
             <Header className="header">
-                <div className="logo" />
+                <img src={logo} alt='na' className={classes.logo}/>
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['2']}
+                    defaultSelectedKeys={['1']}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu.Item key="1"><Icon type="bank" />Home</Menu.Item>                    
+                    <SubMenu title={<span><Icon type="search" />Search</span>}>
+                        <Menu.Item key="search:1">Book search</Menu.Item>
+                        <Menu.Item key="search:2">Asset search</Menu.Item>
+                    </SubMenu>
+
+                    <Menu.Item key="2"><Icon type="book" />Issue</Menu.Item>
+                    <Menu.Item key="3"><Icon type="layout" />Return</Menu.Item>
+
+                    <SubMenu className={classes.menuitem_right} title={<Icon type="idcard" />}>
+                    <Menu.Item key="github"><Icon type="code-o" />Site source</Menu.Item>
+                        <ItemGroup title='You' className={classes.light_item_group}>
+                            <Menu.Item key="user:1"><Icon type="user" />Profile</Menu.Item>
+                            <Menu.Item key="user:2"><Icon type="logout" />Logout</Menu.Item>
+                        </ItemGroup>
+                    </SubMenu>
                 </Menu>
             </Header>
         )
