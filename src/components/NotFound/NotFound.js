3eba4a186d94ff7371ec5ac6e68363f3d313aeb4
kunver400 <kunver400@gmail.com>
2018-05-12 02:02:46 +0530

    mem leak fix

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,21 +5,16 @@ import './NotFound.css';
 
 class NotFound extends Component {
     state = {
-        redirect: false,
         timer: 10
     }
+    timeoutRef;
     componentDidMount() {
-        setInterval(()=>{
+        this.timeoutRef = setInterval(()=>{
         let timer = this.state.timer;
         this.setState({
             timer: --timer
         })
         },1000);
-        setTimeout(()=>{
-            this.setState({
-                redirect: true
-            })
-        },this.state.timer*1000);
     }
     render() {
         return (
@@ -27,7 +22,7 @@ class NotFound extends Component {
                 <h1>error 404: </h1>
                 <h3>You've stumbled upon nothing. </h3>
                 <p>Redirecting...({this.state.timer})</p>
-                {this.state.redirect?<Redirect to="/" />:null}
+                {this.state.timer===0 && !clearTimeout(this.timeoutRef)?<Redirect to="/" />:null}
             </Aux>
         )
     }
