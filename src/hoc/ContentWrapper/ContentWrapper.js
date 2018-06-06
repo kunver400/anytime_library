0839df4ccf6e2b3db9c1dcaec8144f50e31470a9
Kunal Verma <M1039298@mindtree.com>
2018-06-06 11:54:19 +0530

    breadcrumbs and stuff

--- src/hoc/ContentWrapper/ContentWrapper.js
+++ src/hoc/ContentWrapper/ContentWrapper.js
@@ -14,22 +14,27 @@ class ContentWrapper extends Component {
     render() {
         return (
             <Content className={classes.content}>
-                <Breadcrumb>
-                <Breadcrumb.Item>
-                    <Icon type="home" />
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>
-                    <span>All Books</span>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>
-                    <span>Indexed collection</span>
-                </Breadcrumb.Item>
-                </Breadcrumb>
+
                 <Switch>
-                <Route path="/signup" component={SignUp} />
-                <Route path="/indexofbooks" render={()=><IndexedCollection user={this.props.user}/>}/>
-                <Route path="/" exact render={()=><Landing user={this.props.user}/>}/>
-                <Route component={NotFound}/>
+                    <Route path="/signup" render={() => <Breadcrumb>
+                        <Breadcrumb.Item><Icon type="home" /></Breadcrumb.Item>
+                        <Breadcrumb.Item><span>Join us</span></Breadcrumb.Item></Breadcrumb>
+                    } />
+                    <Route path="/indexofbooks" render={() => <Breadcrumb>
+                        <Breadcrumb.Item><Icon type="home" /></Breadcrumb.Item>
+                        <Breadcrumb.Item><span>All Books</span></Breadcrumb.Item>
+                        <Breadcrumb.Item><span>Indexed collection</span></Breadcrumb.Item></Breadcrumb>
+                    } />
+                    <Route path="/" render={() => <Breadcrumb>
+                        <Breadcrumb.Item><Icon type="home" /></Breadcrumb.Item></Breadcrumb>
+                    } />
+                </Switch>
+
+                <Switch>
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/indexofbooks" render={() => <IndexedCollection user={this.props.user} />} />
+                    <Route path="/" exact render={() => <Landing user={this.props.user} />} />
+                    <Route component={NotFound} />
                 </Switch>
             </Content>
         )
