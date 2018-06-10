a3ed25de87dd75fe906301a75921702d8440eaf6
kunver400 <kunver400@gmail.com>
2018-06-10 22:47:20 +0530

    issued books page

--- src/components/IssuedBooks/IssuedBooks.js
+++ src/components/IssuedBooks/IssuedBooks.js
@@ -10,51 +10,86 @@ import Issue from '../Issue/Issue';
 
 import common from '../../utils/common';
 import classes from './IssuedBooks.css';
+import usermeta from '../../utils/usermeta';
 
 const columns = [{
     title: 'Title',
     dataIndex: 'title',
     sorter: (a, b) => a.title.localeCompare(b.title),
     key: 0,
-    width: '35%'
+    width: '30%'
 }, {
     title: 'Author',
     dataIndex: 'author',
     sorter: (a, b) => a.title.localeCompare(b.title),
-    key: 1,
-    width: '35%'
+    key: 1
 },
 {
     title: 'Units Issued',
-    dataIndex: 'availablity',
-    sorter: (a, b) => a.availablity - b.availablity,
-    key: 4
-}
+    dataIndex: 'units',
+    sorter: (a, b) => a.units - b.units,
+    key: 2
+},
+{
+    title: 'Return Date',
+    dataIndex: 'rdate',
+    sorter: (a, b) => {
+        let adate = new Date(a.date_string), bdate = new Date(b.date_string);
+        return (adate < bdate ? 1 : (adate === bdate ? 0 : -1));
+    },
+    render: date_string => common.formatDate(date_string),
+    key: 3
+} 
 ];
 
 
 class IssuedBooks extends Component {
     state = {
         loading: false,
-        addBookModalVisisble: false,
-        editBookModalVisisble: false
+        issuedBooks: [],
     };
     selectedBooks = [];
+    issueKey;
     fetchBooks = (params = {}) => {
+        return new Promise((resolve)=>{
             if (this.props.allBooks.length === 0 || params.force) {
                 this.selectedBooks = [];
-                this.setState({ loading: true });
                 Axios.get('/books.json')
                     .then(response => {
                         let formattedResponse = common.formatBooks(response.data);
                         this.props.SetBooks(formattedResponse);
-                        this.setState({loading: false});
+                        resolve();
                     })
                     .catch(response => {
                         console.log('something went wrong.');
                     });
             }
+            else resolve();
+        })
     };
+    fetchIssuedBooks = () => {
+        this.setState({ loading: true });
+        this.fetchBooks()
+        .then((response)=>{
+            usermeta.getIssuedBooks()
+            .then((data)=>{
+                this.issueKey = data.key;
+                let issuedBooks = this.props.allBooks.filter(book => {
+                    let isIssued = false;
+                    data.issuedBooks.forEach(item => {if(item.bkey === book.key) isIssued=true});
+                    return isIssued;
+                })
+                issuedBooks = issuedBooks.map((item,index)=>{
+                    return {
+                        ...item,
+                        units: data.issuedBooks[index].units,
+                        rdate: data.issuedBooks[index].rdate
+                    };
+                })
+                this.setState({issuedBooks: issuedBooks, loading: false});
+            })
+        })
+    }
     rowSelection = {
         onChange: (selectedRowKeys, selectedRows) => {
             this.selectedBooks = selectedRows;
@@ -65,7 +100,7 @@ class IssuedBooks extends Component {
         }),
     };
     componentDidMount() {
-        this.fetchBooks();
+        this.fetchIssuedBooks();
     };
     ifSelected = () => {
         if (this.selectedBooks.length === 0) {
@@ -78,14 +113,15 @@ class IssuedBooks extends Component {
         return (
             <Auxi>
                 <Table columns={columns}
-                    dataSource={this.props.allBooks}
+                    dataSource={this.state.issuedBooks}
                     loading={this.state.loading}
                     rowSelection={this.rowSelection}
                     size='small'
                     pagination={false}
                 />
-                <Button disabled={!this.props.user} className={classes.table_action_button} onClick={() => { this.ifSelected() && this.props.booksIssueModal(this.selectedBooks) }}>Issue</Button>
-                <Issue {...this.props} />
+                <Button disabled={!this.props.user} className={classes.table_action_button} onClick={() => { this.ifSelected() && this.props.booksIssueModal(this.selectedBooks) }}>Return</Button>
+                <Button disabled={!this.props.user} className={classes.table_action_button} onClick={() => { this.ifSelected() && this.props.booksIssueModal(this.selectedBooks) }}>Re-Issue</Button>
+                <Issue {...this.props} reissue={true}/>
             </Auxi>
         )
     }
